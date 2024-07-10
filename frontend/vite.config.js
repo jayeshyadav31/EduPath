fb661dd5b1098bf6718c12f58abd026d8e0ab422
jayeshyadav31 <104785686+jayeshyadav31@users.noreply.github.com>
2024-07-10 22:10:15 +0530

    changement in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:5173,
-    proxy:{
-      '/api':{
-        target:"https://edupath-yrre.onrender.com",
-        changeOrigin:true,
-        secure:false
+  server: {
+    host: '0.0.0.0', 
+    port: process.env.PORT || 5173,
+    proxy: {
+      '/api': {
+        target: 'https://edupath-yrre.onrender.com',
+        changeOrigin: true,
+        secure: false
       }
     }
   }
